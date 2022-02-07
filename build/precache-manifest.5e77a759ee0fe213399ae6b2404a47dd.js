f319c3442760798c03b9ebb3c8b718f281216087
Chetan Komal <76816799+ChetanKomal@users.noreply.github.com>
2022-02-07 17:59:34 +0530

    Add files via upload

---
+++ build/precache-manifest.5e77a759ee0fe213399ae6b2404a47dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12467c78cc1e7c3b4dfb79276c80e0de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7021cf7f4babfc3cceb0",
+    "url": "/static/css/main.e1a3a49a.chunk.css"
+  },
+  {
+    "revision": "34a1b07da90d063d46c3",
+    "url": "/static/js/2.8a52a507.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.8a52a507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7021cf7f4babfc3cceb0",
+    "url": "/static/js/main.44a5f553.chunk.js"
+  },
+  {
+    "revision": "b6b7328ed845ec8d67bd",
+    "url": "/static/js/runtime-main.e8880884.js"
+  },
+  {
+    "revision": "bf97074f101c33101144b74be13e8732",
+    "url": "/static/media/printable-phone-list-template_40303.bf97074f.png"
+  }
+]);
